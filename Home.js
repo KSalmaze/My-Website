3878cadcf3b6fc6e5a3519e8da9feb6824bc0ca4
KSalmaze <pedrohsalmaze@gmail.com>
2024-04-19 18:34:24 -0300

    Asc art da home

---
+++ Home.js
@@ -0,0 +1,12 @@
+function exibirAscii() {
+    fetch('/AscArts/lightMode.txt')
+        .then(response => response.text())
+        .then(ascii => {
+            // Exibe o conteúdo na div
+            let asciiDiv = document.getElementById('asc-art');
+            asciiDiv.innerText = ascii;
+        })
+        .catch(error => {
+            console.error('Erro ao carregar o arquivo de texto:', error);
+        });
+}
